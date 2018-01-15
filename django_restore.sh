799e91920bfacfc428b7016a45d46f6a7774f9da
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2018-01-15 20:33:31 +0100

    - Django Restore bash script

--- django_restore.sh
+++ django_restore.sh
@@ -15,7 +15,7 @@ case $1 in
 			*)
 				echo "Starting Restore from $2"
 				DJANGO_SETTINGS_MODULE=igad_geonode.settings python -W ignore manage.py restore --backup-file=$2
-				DJANGO_SETTINGS_MODULE=igad_geonode.settings python -W ignore manage.py migrate_baseurl -f --source-address=igad.geo-solutions.it --target-address=igad-dev.geo-solutions.it
+				DJANGO_SETTINGS_MODULE=igad_geonode.settings python -W ignore manage.py migrate_baseurl -f --source-address=igad-dev.geo-solutions.it --target-address=igad.geo-solutions.it
 				;;
 		esac
 		;;
