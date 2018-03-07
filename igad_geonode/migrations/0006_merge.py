3153d3b57eb68be0c4976ac9ce0e67a5789ce7ab
Cezary Statkiewicz <cezio@thelirium.net>
2018-03-07 16:23:03 +0100

    merge migrations

---
+++ igad_geonode/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('igad_geonode', '0005_menu_item_region'),
+        ('igad_geonode', '0005_menu_location'),
+    ]
+
+    operations = [
+    ]
